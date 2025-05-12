b8a32030fb5ca7ecc67cc938d342d09cc593a76f
dengjie5 <dengjie5@xiaomi.com>
2025-05-12 15:22:06 +0800

    feat: add todo

--- rn-movie-app/app/index.tsx
+++ rn-movie-app/app/index.tsx
@@ -4,6 +4,7 @@ import './globals.css';
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center">
+      {/* TODO 样式不生效 */}
       <Text className="text-5xl text-blue-500">Welcome!</Text>
     </View>
   );
