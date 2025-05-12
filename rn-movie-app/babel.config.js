c26d5d25979be233858ce42a9760478c4e4a303c
dengjie5 <dengjie5@xiaomi.com>
2025-05-12 14:59:38 +0800

    feat: 修改基础配置

--- rn-movie-app/babel.config.js
+++ rn-movie-app/babel.config.js
@@ -2,11 +2,8 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
-      ["babel-preset-expo", { 
-        jsxImportSource: "nativewind",
-        jsxRuntime: "automatic" 
-      }]
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
     ],
-    plugins: ["nativewind/babel"]
   };
 };
